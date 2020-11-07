78cdf0da3d0dd1f52f10624d9ef84d7c91a5ba0a
Irek Sołek <kashel@MacBook-Pro-Ireneusz.local>
2020-11-07 17:33:06 +0100

    Initial commit

---
+++ Ethanol/EthanolApp.swift
@@ -0,0 +1,17 @@
+//
+//  EthanolApp.swift
+//  Ethanol
+//
+//  Created by Ireneusz Sołek on 02/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct EthanolApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}

b809e51d018157174954606c814fb16a6f2526d0
Mihály Papp <michal@papp.pl>
2020-11-14 18:37:05 +0100

    Make DependencyContainer injectable

--- Ethanol/Injected/DependencyContainer.swift
+++ Ethanol/Injected/DependencyContainer.swift
@@ -1,11 +1,19 @@
+import SwiftUI
 
 struct DependencyContainer {
   let appState = Store<AppState>(AppState())
   let interactors: Interactors
 }
 
-extension DependencyContainer {
-  struct Interactors {
+extension DependencyContainer: EnvironmentKey {
+  static var defaultValue: DependencyContainer {
+    DependencyContainer(interactors: Interactors())
+  }
+}
 
+extension EnvironmentValues {
+  var injected: DependencyContainer {
+    get { self[DependencyContainer.self] }
+    set { self[DependencyContainer.self] = newValue }
   }
 }

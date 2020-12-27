36d2d1b279447ae18253b11a10522ac9f6505d48
Irek Sołek <intrek@gmail.com>
2020-12-27 22:50:52 +0100

    Ingredients

--- Ethanol/IngredientsSelection/Models/IngredientGroup.swift
+++ Ethanol/IngredientsSelection/Models/IngredientGroup.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 enum IngredientGroup: String, CaseIterable {
-  case alcohols, fruits, common, herbs, spices
+  case alcohols, fruits, common, herbs, spices, sweets, decors, juices
   var name: String { rawValue }
 }

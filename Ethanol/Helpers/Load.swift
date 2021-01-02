d394836494f4ee41c375a8cd09be396be2af3ea1
Irek Sołek <intrek@gmail.com>
2021-01-02 15:19:02 +0100

    Refactor load functions

--- Ethanol/Helpers/Load.swift
+++ Ethanol/Helpers/Load.swift
@@ -1,23 +1,25 @@
 import Foundation
 
 func load<T: Decodable>(_ filename: String, extension: String? = nil) -> T {
-    let data: Data
-
     guard let file = Bundle.main.url(forResource: filename, withExtension: `extension`)
     else {
         fatalError("Couldn't find \(filename) in main bundle.")
     }
+  return load(url: file)
+}
 
-    do {
-        data = try Data(contentsOf: file)
-    } catch {
-        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
-    }
+func load<T: Decodable>(url file: URL) -> T {
+  let data: Data
+  do {
+      data = try Data(contentsOf: file)
+  } catch {
+    fatalError("Couldn't load \(file.absoluteString) from main bundle:\n\(error)")
+  }
 
-    do {
-        let decoder = JSONDecoder()
-        return try decoder.decode(T.self, from: data)
-    } catch {
-        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
-    }
+  do {
+      let decoder = JSONDecoder()
+      return try decoder.decode(T.self, from: data)
+  } catch {
+      fatalError("Couldn't parse \(file.absoluteString) as \(T.self):\n\(error)")
+  }
 }
